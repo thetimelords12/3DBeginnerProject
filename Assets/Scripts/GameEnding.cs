4a425641eee1f0998fa32c5d0e3b15d9c4620581
William Loaiza <mothermayi2014@gmail.com>
2020-12-02 20:36:59 -0600

    Fixes

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -67,3 +67,4 @@ public class GameEnding : MonoBehaviour
         }
     }
 }
+
